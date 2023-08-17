b30f9bfbc9b07a53693a7d15b2428abd423a7531
User <imaxik775@yandex.ru>
2023-08-17 09:20:07 +0300

    Initial Commit

---
+++ Rick&Mortty App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Mortty App
+//
+//  Created by Максим Целигоров on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
